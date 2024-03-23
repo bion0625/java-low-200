56af53e647da90229bed3b12185a3343eb3facb6
bion0625 <stxtory@gmail.com>
2024-03-23 10:47:16 +0900

    todo 표시 제거

---
+++ UsingClass.java
@@ -0,0 +1,7 @@
+public class UsingClass {
+    public static void main(String[] args){
+        Object obj1 = new Object();
+        Class classes = obj1.getClass();
+        System.out.println(classes.getName());
+    }
+}
