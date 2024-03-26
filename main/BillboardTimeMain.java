35ee199856bafa0ea5074086b9711ea3aad4d99b
bion0625 <stxtory@gmail.com>
2024-03-26 13:02:17 +0900

    웹 크롤링 특정 문자열만 추출 기능

---
+++ main/BillboardTimeMain.java
@@ -0,0 +1,34 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class BillboardTimeMain {
+    public static void main(String[] args) {
+        String newUrls = "https://www.billboard.com/charts/hot-100/";
+        String s = "";
+        URL url = null;
+        try{
+            url = new URL(newUrls); // 주소 찾기
+            // 주소지에 빨대 꽂기
+            BufferedReader reader = new BufferedReader(
+                new InputStreamReader(url.openStream(), "euc-kr"), 8);
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    if(!line.trim().equals("")) {
+                        // <time datetime="2018-08-13">August 13, 2018</time>
+                        if (line.trim().contains("mega_menu_bb_mustread_opt_in_date")) { // 찾기
+                            s=line.trim();
+                            s = s.substring(0, s.indexOf("\" />")).trim(); // <time datetime="2018-08-13"
+                            s = s.substring(s.indexOf("value=") + 7).trim(); // 2018-08-13
+                            break;
+                        }
+                    }
+                }
+        }catch(Exception e){
+            System.out.println("Billboard Parsing error !!!");
+        }
+        System.out.println("이번 빌보드 차트 날짜:" + s);
+    }
+}
--- mySample/PlayGround.java
+++ mySample/PlayGround.java
@@ -5,8 +5,10 @@ import java.util.Date;
 
 public class PlayGround {
     public static void main(String[] args) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        String dayString = sdf.format(new Date());
-        System.out.println(dayString);
+        String s = "<input type=\"hidden\" name=\"BB_MustRead_Opt_In_Date\" id=\"mega_menu_bb_mustread_opt_in_date\" value=\"2024-03-26\" />";
+        s = s.substring(0, s.indexOf("\" />"));
+        s = s.substring(s.indexOf("value=") + 7);
+
+        System.out.println(s);
     }
 }
