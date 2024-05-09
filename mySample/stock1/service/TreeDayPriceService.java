b4029d9289c35023a1c3e05c97c0ea65c63a8160
bion0625 <bion0625@gmail.com>
2024-05-09 16:56:20 +0900

    10일 중 신고가 기능 추가

--- mySample/stock1/service/TreeDayPriceService.java
+++ mySample/stock1/service/TreeDayPriceService.java
@@ -3,6 +3,7 @@ package mySample.stock1.service;
 import java.util.List;
 
 import mySample.stock1.Info;
+import mySample.stock1.model.Price;
 import mySample.stock1.model.Stock;
 
 // 삼일 연달아 상승 가격 종목 구해오기
@@ -17,7 +18,12 @@ public class TreeDayPriceService {
         List<Stock> stocks = info.getCompanyInfo();
 
         for (Stock stock : stocks) {
-            stock.setPrices(info.getPriceInfo(stock.getCode(), 1).subList(0, 3));
+            List<Price> prices = info.getPriceInfo(stock.getCode(), 1);
+            // 10일 중 신고가
+            Price checkPrice = prices.stream().reduce((p, c) -> p.getHigh() > c.getHigh() ? p : c).orElse(null);
+            if(prices.get(0).getHigh() == checkPrice.getHigh()) {
+                stock.setPrices(prices.subList(0, 3));
+            }
 
             // 로그
             System.out.println(String.format("%d/%d", stocks.indexOf(stock), stocks.size()));
@@ -31,6 +37,11 @@ public class TreeDayPriceService {
             && s.getPrices().get(1).getLow() > s.getPrices().get(2).getLow();
         }).toList();
 
-        System.out.println(stocks);
+        for (Stock stock : stocks) {
+            System.out.println(stock.getName());
+            System.out.println("날짜: " + stock.getPrices().get(0).getDate());
+            System.out.println("종가: " + stock.getPrices().get(0).getClose());
+            System.out.println("고가: " + stock.getPrices().get(0).getHigh());
+        }
     }
 }
