6fd697a85da327a525931b609c98d00777cd6a39
bion0625 <bion0625@gmail.com>
2024-05-10 09:43:15 +0900

    당일 diff가 5% ~ 15% 내에 있지 않으면 제외 로직 추가

--- mySample/stock1/service/TreeDayPriceService.java
+++ mySample/stock1/service/TreeDayPriceService.java
@@ -11,6 +11,8 @@ import mySample.stock1.util.FormatUtil;
 /*
  * 1) 3일 연달아 가격 상승이 아니면 제외
  * 2) 오늘이 거래량 최고면 제외
+ * 3) 기준 날짜 대비 신고가 아니면 제외
+ * 4) 당일 diff가 5% ~ 15% 내에 있지 않으면 제외
 */
 public class TreeDayPriceService {
     public static void main(String[] args) {
@@ -19,43 +21,66 @@ public class TreeDayPriceService {
     }
 
     public void start() {
+        // 신고가 개월 수 (페이지 계산식 항)
+        int MONTH = 3;
+        /*
+         * 13 페이지 6개월
+         * 1페이지 10일 (2주)
+         * 2페이지 1개월
+         * 4페이지 2개월
+         * 6페이지 3개월
+         * ..
+        */
+        int SEARCH_PAGE = 2 * MONTH;
+
         Info info = new Info();
         List<Stock> stocks = info.getCompanyInfo();
 
         for (Stock stock : stocks) {
             // 로그
-            System.out.println(String.format("%d/%d", stocks.indexOf(stock)+1, stocks.size()));
+            System.out.println(String.format("%d\t/\t%d", stocks.indexOf(stock)+1, stocks.size()));
 
-            List<Price> prices = info.getPriceInfoByPage(stock.getCode(), 1, 10);
-            // 100일 중 신고가가 아니면 제외
-            Price checkPrice = prices.stream().reduce((p, c) -> p.getHigh() > c.getHigh() ? p : c).orElse(null);
+            // 부하를 방지하기 위해 일단 1페이지만 확인 후 신고가 설정할 때 다시 구하기
+            List<Price> prices = info.getPriceInfo(stock.getCode(), 1);
+
+            // 거래량이 0이면 하루 전으로 계산
+            int lastdayIndex = prices.get(0).getVolume() == 0 ? 1 : 0;
+
+            // 최근 3일 중 마지막일이 최고 거래량이면 제외            
+            Price checkPrice = prices.subList(lastdayIndex, (lastdayIndex + 3)).stream().reduce((p, c) -> p.getVolume() > c.getVolume() ? p : c).orElse(null);
+            if (checkPrice != null && prices.get(0).getVolume() == checkPrice.getVolume()) continue;
+
+            // 마지막일 diff가 5% ~ 15% 내에 있지 않으면 제외
+            double diffPercent = (double) (prices.get(lastdayIndex).getDiff() * 100) / (double) prices.get(lastdayIndex + 1).getClose();
+            if (diffPercent < 5 || diffPercent > 15) continue;
+
+             // 부하를 방지하기 위해 신고가 설정할 때 다시 구하기
+            prices = info.getPriceInfoByPage(stock.getCode(), 1, SEARCH_PAGE);
+
+            // 6개월 중 신고가가 아니면 제외
+            checkPrice = prices.stream().reduce((p, c) -> p.getHigh() > c.getHigh() ? p : c).orElse(null);
             if(checkPrice == null || prices.get(0).getHigh() != checkPrice.getHigh()) continue;
-            
-            // 3일치 가격만 저장
-            stock.setPrices(prices.subList(0, 3));
 
-            // 최근 3일 중 오늘이 최고 거래량이면 제외
-            checkPrice = stock.getPrices().stream().reduce((p, c) -> p.getVolume() > c.getVolume() ? p : c).orElse(null);
-            if (checkPrice != null && stock.getPrices().get(0).getVolume() == checkPrice.getVolume()) continue;
+            // 3일 연달아 가격 상승이 아니면 제외
+            if(prices.get(lastdayIndex).getHigh() <= prices.get(lastdayIndex + 1).getHigh() || prices.get(lastdayIndex + 1).getHigh() <= prices.get(lastdayIndex + 2).getHigh()
+            || prices.get(lastdayIndex).getLow() <= prices.get(lastdayIndex + 1).getLow() || prices.get(lastdayIndex + 1).getLow() <= prices.get(lastdayIndex + 2).getLow()) {
+                continue;
+            }
+
+            // 리스트에 저장
+            stock.setPrices(prices);
+
+            // 로그
+            System.out.println(String.format("\tsuccess:\t%s", stock.getName()));
         }
 
-        // 3일 연달아 가격 상승이 아니면 제외
-        stocks = stocks.stream()
-        .filter(s -> {
-            return s.getPrices() != null
-            && s.getPrices().get(0).getHigh() > s.getPrices().get(1).getHigh()
-            && s.getPrices().get(1).getHigh() > s.getPrices().get(2).getHigh()
-            && s.getPrices().get(0).getLow() > s.getPrices().get(1).getLow()
-            && s.getPrices().get(1).getLow() > s.getPrices().get(2).getLow();
-        })
-        .collect(Collectors.toList());
+        // 가격 정보가 있는 객체만 남긴다.
+        stocks = stocks.stream().filter(s -> s.getPrices() != null).collect(Collectors.toList());
 
         for (Stock stock : stocks) {
             FormatUtil formatUtil = new FormatUtil();
-            System.out.println(stock.getName());
-            System.out.println("날짜: " + formatUtil.dateToString(stock.getPrices().get(0).getDate()));
-            System.out.println("종가: " + stock.getPrices().get(0).getClose());
-            System.out.println("고가: " + stock.getPrices().get(0).getHigh());
-        }
+            System.out.println(String.format("%s\n\t%s", 
+                stock.getName(), formatUtil.dateToString(stock.getPrices().get(0).getDate())));
+        }                   
     }
 }
--- mySample/stock1/util/FormatUtil.java
+++ mySample/stock1/util/FormatUtil.java
@@ -15,9 +15,9 @@ public class FormatUtil {
         String str = null;
         try {
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-            str = sdf.format(date);
+            str = date == null ? null : sdf.format(date);
         } catch (Exception e) {
-            System.out.println(String.format("date >>>> %s", date));
+            System.out.println(String.format("dateToString : date >>>> %s", date));
         }
         return str;
     }
@@ -26,20 +26,21 @@ public class FormatUtil {
         Date date = null;
         try {
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
-            date = sdf.parse(str);
+            date = str == "" ? null : sdf.parse(str);
         } catch (Exception e) {
-            System.out.println(String.format("str >>>> %s", str));
+            System.out.println(String.format("stringToDate : str >>>> %s", str));
         }
         return date;
     }
 
     public long stringToLong(String str) {
         long result = 0L;
+        str = str == "" ? "0" : str;
         try {
             str = str.replaceAll(",", "");
             result = Long.parseLong(str);
         } catch (Exception e) {
-            System.out.println(String.format("str >>>> %s", str));
+            System.out.println(String.format("stringToLong : str >>>> %s", str));
         }
         return result;
     }
