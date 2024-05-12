2d90026a94fabaa81c8b5bda768482f65d9f81dd
이의정 <stxtory@gmail.com>
2024-05-12 23:39:44 +0900

    로그 기능 수정

--- mySample/stock1/controller/TimerController.java
+++ mySample/stock1/controller/TimerController.java
@@ -11,6 +11,9 @@ import mySample.stock1.service.GetStockAndSendService;
 import mySample.stock1.service.SendMailService;
 
 public class TimerController {
+    String LOG_THIS_TEXT = "This time is %d";
+    String LOG_NEXT_TEXT = "Next time is %d";
+
     GetStockAndSendService getStockAndSendService = new GetStockAndSendService();
     SendMailService sendMailService = new SendMailService();
 
@@ -22,12 +25,18 @@ public class TimerController {
                 List<Integer> actTimes = new ArrayList<>(Arrays.asList(8,11,14,17));
                 LocalDateTime now = LocalDateTime.now();
                 if (actTimes.contains(now.getHour())) {
-                    System.out.println(String.format("this time is %d", now.getHour()));
+                    System.out.println(String.format(LOG_THIS_TEXT, now.getHour()));
                     // sendMailService.mainInfoSetting(); // 메일 세팅 정보 (처음에만 한 번)
                     getStockAndSendService.start();
                     if (actTimes.indexOf(now.getHour()) == actTimes.size() - 1) {
-                        System.out.println(String.format("next time is %d", actTimes.get(0)));
-                    } else System.out.println(String.format("next time is %d", actTimes.get(actTimes.indexOf(now.getHour()) + 1)));
+                        System.out.println(String.format(LOG_NEXT_TEXT, actTimes.get(0)));
+                    } else System.out.println(String.format(LOG_NEXT_TEXT, actTimes.get(actTimes.indexOf(now.getHour()) + 1)));
+                } else {
+                    int nextTime = actTimes.get(0);
+                    for (int i = actTimes.size() - 1; i >= 0; i--) {
+                        nextTime = now.getHour() < actTimes.get(i) ? actTimes.get(i) : nextTime;
+                    }
+                    System.out.println(String.format(LOG_NEXT_TEXT, nextTime));
                 }
             }
         };
