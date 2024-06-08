73e6e9a37dc302c0513835baee22dd1637139f9e
bion0625 <bion0625@gmail.com>
2024-06-08 18:14:20 +0900

    126: Calendar를 이용하여 휴일(토요일, 일요일) 구하기

---
+++ main/base/RestDay2.java
@@ -0,0 +1,33 @@
+package main.base;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+// s075 RestDay 추가
+public class RestDay2 {
+    // 토, 일요일인가?
+    public static boolean isRest(Calendar tod) {
+        boolean isRest = false;
+        if (tod.get(Calendar.DAY_OF_WEEK)==Calendar.SATURDAY || // 토요일
+            tod.get(Calendar.DAY_OF_WEEK)==Calendar.SUNDAY) { // 또는 일요일
+            isRest=true;
+        }
+        return isRest;
+    }
+
+    // Calendar를 문자열로
+    public static String toYND(Calendar dd) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        return sdf.format(dd.getTime());
+    }
+
+    public static void main(String[] args) {
+        Calendar cal=Calendar.getInstance();
+        int lastDay=cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+        for (int i = 1; i < lastDay; i++) {
+            cal.set(Calendar.DAY_OF_MONTH, i); // 1일부터 마지막 날까지
+            if (isRest(cal)) { // 휴일(토, 일) 확인
+                System.out.println(toYND(cal) + " is Rest Day");
+            }
+        }
+    }
+}
