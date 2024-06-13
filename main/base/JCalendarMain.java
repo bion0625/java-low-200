dc9d82e40c60c6864b57eed6b43f23dcb481b250
bion0625 <bion0625@gmail.com>
2024-06-13 10:06:23 +0900

    129: 사용자 정의 만년 달력 만들기

---
+++ main/base/JCalendarMain.java
@@ -0,0 +1,9 @@
+package main.base;
+
+public class JCalendarMain {
+    public static void main(String[] args) {
+        JCalendar cal=new JCalendar();
+        // cal.showCal(2024,6);
+        cal.showCal(2024);
+    }
+}
