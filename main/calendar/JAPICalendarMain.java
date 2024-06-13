86cababea98725e6acf389f9a8a5ec4127ad4ef1
bion0625 <bion0625@gmail.com>
2024-06-13 10:26:33 +0900

    130: Calendar를 이용하여 만년 달력 만들기

---
+++ main/calendar/JAPICalendarMain.java
@@ -0,0 +1,9 @@
+package main.calendar;
+
+public class JAPICalendarMain {
+    public static void main(String[] args) {
+        JAPICalendar haecal = new JAPICalendar();
+        // haecal.showCal(2024,6);
+        haecal.showCal(2024);
+    }
+}
